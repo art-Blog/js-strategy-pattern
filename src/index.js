712482922ed3962d823796c0617aa8e63c1126be
art <partypeopleland@gmail.com>
2018-06-19 18:12:43 +0800

    改寫es6語法，分離config從外部注入

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 
 
-var validator = {
-    types: {},
-    messages: [],
-    config: {},
-
-    validate: function (data) {
+class Validator {
+    constructor(config) {
+        this.types = config
         this.messages = []
-        for (i in data) {
+        this.config = {}
+    }
+
+    validate(data) {
+        this.messages = []
+        for (let i in data) {
             // 逐一取得物件所有的屬性
             if (data.hasOwnProperty(i)) {
-                type = this.config[i]
-                checker = this.types[type]
+                // console.log(i)
+                let type = this.config[i]
+                let checker = this.types[type]
 
                 if (!type) { continue }
 
@@ -20,43 +23,42 @@ var validator = {
                         name: "ValidationError", message: "No handler to validate type:" + type
                     }
                 }
-                result = checker.validate(data[i])
+                let result = checker.validate(data[i])
                 if (!result) {
-                    msg = "Invalid value for *" + i + "*, " + checker.instructions
+                    let msg = "Invalid value for *" + i + "*, " + checker.instructions
                     this.messages.push(msg)
                 }
             }
         }
         return this.hasErrors()
-    },
-    hasErrors: function () {
+    }
+    hasErrors() {
         return this.messages.length !== 0
     }
 
 }
 
-validator.types.isNonEmpty = {
-    validate: function (value) {
-        return value !== ""
+
+const config = {
+    isNonEmpty: {
+        validate: function (value) {
+            return value !== ""
+        },
+        instructions: "the value cannot be empty"
     },
-    instructions: "the value cannot be empty"
-}
-
-validator.types.isNumber = {
-    validate: function (value) {
-        return !isNaN(value)
+    isNumber: {
+        validate: function (value) {
+            return !isNaN(value)
+        },
+        instructions: "the value can only be a valid number, e.g. 1, 3.14 or 201"
     },
-    instructions: "the value can only be a valid number, e.g. 1, 3.14 or 201"
+    isAlphaNum: {
+        validate: function (value) {
+            return !/[^a-z0-9]/i.test(value)
+        },
+        instructions: "the value can only contain characters and numbers, no spe"
+    }
 }
-
-validator.types.isAlphaNum = {
-    validate: function (value) {
-        return !/[^a-z0-9]/i.test(value)
-    },
-    instructions: "the value can only contain characters and numbers, no spe"
-}
-
-
 
 var data = {
     firstName: "Super",
@@ -65,6 +67,7 @@ var data = {
     userName: "o_O"
 }
 
+let validator = new Validator(config)
 validator.config = {
     firstName: "isNonEmpty",
     age: "isNumber",
